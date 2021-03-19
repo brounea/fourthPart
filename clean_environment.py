32ad0f06b02dc82f5048f01e6131ab1653c92163
Arnon <brounera@gmail.com>
2021-03-19 15:16:06 +0200

    fixed str

--- clean_environment.py
+++ clean_environment.py
@@ -1,3 +1,32 @@
 import requests
 
-requests.get('http://127.0.0.1:5000/stop_server')
+def rm_test_user():
+    # Delete test user from the automation
+    try:
+        requests.delete('http://localhost:5000/users/666')
+        print(" post delete user action")
+    except requests.exceptions.ConnectionError as e:
+        print("Connection refused to rest_app service", e)
+
+
+def rm_specific_user(id):
+    # Delete specific user from the automation
+    try:
+        requests.delete('http://localhost:5000/users/%s' % id)
+        print("user %s" % id + " has been deleted")
+    except requests.exceptions.ConnectionError as e:
+        print("Connection refused to rest_app service", e)
+
+
+def stop_rest_app():
+    # Stop rest app service
+    try:
+        requests.get('http://localhost:5000/stop_server')
+    except requests.exceptions.ConnectionError as e:
+        print("Connection refused to rest_app service", e)
+
+
+if __name__ == "__main__":
+    rm_test_user()
+    stop_rest_app()
+
